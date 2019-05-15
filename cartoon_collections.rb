f9fb6d9a59538348c1a536d5ed0073d736b03d6d
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-15 06:18:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.any? do |snack|
+  array.find do |snack|
     cheese_types.include?(snack)
   end
 end
